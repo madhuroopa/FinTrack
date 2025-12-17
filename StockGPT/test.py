75632e43441d5fb09f1e803e7c7b44524134686a
Madhuroopa <madhuroopa246@gmail.com>
2025-12-16 19:15:58 -0800

    Update test.py

--- StockGPT/test.py
+++ StockGPT/test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = ('https://newsapi.org/v2/everything?q=AAPL&from=2024-10-19&sortBy=popularity&apiKey=3b3859b565604d55919a20c5421747a2')
+url = ('https://newsapi.org/v2/everything?q=AAPL&from=2024-10-19&sortBy=popularity&apiKey=')
 
 response = requests.get(url)
 
